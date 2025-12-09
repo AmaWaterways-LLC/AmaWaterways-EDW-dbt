ec1dd664e6ca59d3b5e0e658a51be412fff00798
itdataengineering <itdataengineering@amawaterways.com>
2025-12-09 10:22:59 +0530

    replacing dbt sql models

---
+++ models/silver/seaware/silver__ship_deck.sql
@@ -0,0 +1,120 @@
+{# ================================================================
+   Generate Batch ID (must be top-of-file, before config)
+   ================================================================ #}
+{% set batch_id = invocation_id ~ '-' ~ this.name ~ '-' ~ modules.datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%S%fZ') %}
+
+{# ================================================================
+   CONFIG BLOCK
+   ================================================================ #}
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy = 'merge',
+        unique_key=['DECK_NUMBER', 'SHIP_CODE'],
+        pre_hook=[
+            "{% set target_relation = adapter.get_relation(database=this.database, schema=this.schema, identifier=this.name) %}
+             {% set table_exists = target_relation is not none %}
+             {% if table_exists %}
+                 {% set cfg = get_config_row('SW1', target.database, target.schema, 'SHIP_DECK') %}
+                 {% set load_type_val = 'FULL' if cfg['LAST_UPDATED_WATERMARK_VALUE'] is none else 'INCREMENTAL' %}
+                 {% do audit_start(
+                     pipeline_name='bronze_to_silver_ship_deck',
+                     source_name='AUTO',
+                     database_name=target.database,
+                     schema_name=target.schema,
+                     table_name='SHIP_DECK',
+                     layer='SILVER',
+                     operation_type='MERGE',
+                     load_type=load_type_val,
+                     environment=target.name,
+                     ingested_by='dbt',
+                     batch_id='" ~ batch_id ~ "'
+                 ) %}              
+             {% endif %}"
+        ],
+        post_hook=[
+            "{% do audit_update_counts(batch_id='" ~ batch_id ~ "', record_count_target=get_record_count(this)) %}",
+            "{% do audit_end(batch_id='" ~ batch_id ~ "', status='SUCCESS') %}",
+            "{% if execute %}
+                 {% set wm_col_sw1 = get_watermark_column('SW1', target.database, target.schema, 'SHIP_DECK') %}
+                 {% set max_wm_sw1 = compute_max_watermark_seaware(this, wm_col_sw1, 'SW1') %}
+                 {% if max_wm_sw1 is not none %}
+                     {% do update_config_watermark('SW1', target.database, target.schema, 'SHIP_DECK', max_wm) %}
+                 {% endif %}
+
+                {% set wm_col_sw2 = get_watermark_column('SW2', target.database, target.schema, 'SHIP_DECK') %}
+                 {% set max_wm_sw2 = compute_max_watermark_seaware(this, wm_col_sw2, 'SW2') %}
+                 {% if max_wm_sw2 is not none %}
+                     {% do update_config_watermark('SW2', target.database, target.schema, 'SHIP_DECK', max_wm) %}
+                 {% endif %}
+             {% endif %}"
+        ]
+    )
+}}
+
+{# ================================================================
+   FETCH CONFIG & WATERMARK INFO
+   ================================================================ #}
+
+{% if execute %}
+    {% set cfg_sw1 = get_config_row('SW1', target.database, target.schema, 'SHIP_DECK') %}
+    {% set wm_col_sw1 = cfg_sw1['WATERMARK_COLUMN'] %}
+    {% set last_wm_sw1 = cfg_sw1['LAST_UPDATED_WATERMARK_VALUE'] %}
+    {% set is_full_sw1 = (last_wm is none) %}
+    {% set cfg_sw2 = get_config_row('SW2', target.database, target.schema, 'SHIP_DECK') %}
+    {% set wm_col_sw2 = cfg_sw2['WATERMARK_COLUMN'] %}
+    {% set last_wm_sw2 = cfg_sw2['LAST_UPDATED_WATERMARK_VALUE'] %}
+    {% set is_full_sw2 = (last_wm is none) %}
+{% else %}
+    {% set wm_col_sw1 = none %}
+    {% set last_wm_sw1 = none %}
+    {% set is_full_sw1 = true %}
+    {% set wm_col_sw2 = none %}
+    {% set last_wm_sw2 = none %}
+    {% set is_full_sw2 = true %}
+{% endif %}
+
+{# ================================================================
+   SOURCE CTE
+   ================================================================ #}
+
+with sw1_src as (
+    select
+    'SW1' AS DATA_SOURCE,
+{{ transform_string('SHIP_CODE') }} AS SHIP_CODE,
+{{ transform_numeric('DECK_NUMBER') }} AS DECK_NUMBER,
+{{ transform_numeric('DECK_ID') }} AS DECK_ID,
+{{ transform_string('DECK_NAME') }} AS DECK_NAME,
+{{ transform_string('IS_DECK_PAX') }} AS IS_DECK_PAX,
+{{ transform_numeric('IMAGE_ID') }} AS IMAGE_ID,
+{{ transform_string('DECK_CODE') }} AS DECK_CODE,
+{{ transform_numeric('LAYOUT_IMAGE_ID') }} AS LAYOUT_IMAGE_ID,
+{{ transform_datetime('_FIVETRAN_SYNCED') }} AS LAST_UPDATED_TIMESTAMP,
+ _FIVETRAN_DELETED AS SOURCE_DELETED
+    from {{ source('AMA_PROD_BRNZ_SW1', 'SHIP_DECK') }}
+    {% if is_incremental() and not is_full %}
+    where coalesce({{ wm_col_sw1 }}, {{ wm_default_literal() }}) > {{ _format_watermark(last_wm_sw1) }}
+    {% endif %}
+),
+
+sw2_src as (
+    select
+    'SW2' AS DATA_SOURCE,
+{{ transform_string('SHIP_CODE') }} AS SHIP_CODE,
+{{ transform_numeric('DECK_NUMBER') }} AS DECK_NUMBER,
+{{ transform_numeric('DECK_ID') }} AS DECK_ID,
+{{ transform_string('DECK_NAME') }} AS DECK_NAME,
+{{ transform_string('IS_DECK_PAX') }} AS IS_DECK_PAX,
+{{ transform_numeric('IMAGE_ID') }} AS IMAGE_ID,
+{{ transform_string('DECK_CODE') }} AS DECK_CODE,
+{{ transform_numeric('LAYOUT_IMAGE_ID') }} AS LAYOUT_IMAGE_ID,
+{{ transform_datetime('_FIVETRAN_SYNCED') }} AS LAST_UPDATED_TIMESTAMP,
+ _FIVETRAN_DELETED AS SOURCE_DELETED
+    from {{ source('AMA_PROD_BRNZ_SW2', 'SHIP_DECK') }}
+    {% if is_incremental() and not is_full %}
+    where coalesce({{ wm_col_sw2 }}, {{ wm_default_literal() }}) > {{ _format_watermark(last_wm_sw2) }}
+    {% endif %}
+)
+    select * from sw1_src
+    union all
+    select * from sw2_src
